dfa5ea85808ad36a2e49af0471c081718e333926
Sathish <sathish@sathish-ubuntu.(none)>
2009-07-11 04:04:50 +0530

    sathish: re-implemented 1 and 2 using only head, tail and EMPTY

--- js/1to99.js
+++ js/1to99.js
@@ -1,19 +1,30 @@
+var EMPTY = [];
+
+function head(list){
+  return list[0];
+}
+
+function tail(list){
+  return list.slice(1)
+}
+
+Array.prototype.eq = function(other){
+  return this.length > 0 ? this == other :
+         other.constructor == Array && other.length == 0 //Empty Arrays are not equal in js
+}
+
 //1
 function last(list){
-  if(list.length == 0)
-    return null;
-  else if(list.length == 1)
-    return list[0]
+  if(tail(list).eq(EMPTY))
+    return head(list);
   else
-    return last(list.slice(1))
+    return last(tail(list));
 }
 
 //2
 function last_but_one(list){
-  if(list.length < 2)
-    return null;
-  else if(list.length == 2)
-    return list[0]
+  if(head(tail(list)) == last(list))
+    return head(list);
   else
-    return last_but_one(list.slice(1))
+    return last_but_one(list.slice(1));
 }
