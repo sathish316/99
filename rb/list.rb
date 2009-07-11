cec29137c7b2c89078fa033bb33419f856a53af4
Sathish <sathish@sathish-ubuntu.(none)>
2009-07-11 13:06:56 +0530

    solved till 20 in rb

--- rb/list.rb
+++ rb/list.rb
@@ -37,6 +37,7 @@ def flatten(list)
   end
 end
 
+puts "flatten:"
 p flatten([1,[2,[3,4],5]])
 
 def compress(list)
@@ -45,6 +46,7 @@ def compress(list)
   clist
 end
 
+puts "compress:"
 p compress([1,1,2,2,2,2,3,4,5,6,6,6,6,7,7,7])
 
 def pack(list)
@@ -58,12 +60,14 @@ def pack(list)
   plist
 end
 
+puts "pack:"
 p pack([1,1,2,2,2,2,3,4,5,6,6,6,6,7,7,7])
 
 def rle(list)
   pack(list).collect {|l| [l.size, l.first]}
 end
 
+puts "rle:"
 p rle([1,1,2,2,2,2,3,4,4,5,6,6,6,6,7,7,7])
 
 def rlee(list)
@@ -72,6 +76,7 @@ def rlee(list)
   end
 end
 
+puts "rlee"
 p rlee([1,1,2,2,2,2,3,4,4,5,6,6,6,6,7,7,7])
 
 def uncompress(rlee)
@@ -85,21 +90,96 @@ def uncompress(rlee)
   end
 end
 
+puts "uncompress:"
 p uncompress([[2, 1], [4, 2], 3, [2, 4], 5, [4, 6], [3, 7]])
 
-def rled#TODO
-
+def rled(list)#TODO
+  clist = []
+  num = count = nil
+  list.each do |x|
+    last = clist.last
+    unless (last and (last == x or last[1] == x))
+      clist << x
+    else
+      last = clist.pop
+      if last.is_a? Fixnum
+        clist << [2,x]
+      else
+        clist << [last[0]+1,x]
+      end
+    end
+  end
+  clist
 end
+
+puts "rled:"
+p rled([1,1,2,2,2,2,3,4,4,5,6,6,6,6,7,7,7])
 
 def dup(list)
   list.inject([]){|result,x| result << x; result << x}
 end
 
+puts "dup:"
 p dup([1,2,3,4,5])
 
 def dupn(list,n)
   list.inject([]){|result,x| n.times {result << x}; result}
 end
 
+puts "dupn:"
 p dupn([1,2,3,4,5],4)
 
+class Array
+  def select_with_index(&block)
+    sel_array = []
+    each_with_index do |x,i|
+      sel_array << x if yield(x,i)
+    end
+    sel_array
+  end
+  def reject_with_index(&block)
+    sel_array = []
+    each_with_index do |x,i|
+      sel_array << x unless yield(x,i)
+    end
+    sel_array
+  end
+end
+
+def drop(list,n)
+  list.reject_with_index {|x,index| (index+1) % n == 0}
+end
+
+puts "drop:"
+p drop(['a','b','c','d','e','f','g','h','i'],3)
+
+def split(list, n)
+  first = []
+  n.times {first << list.shift}
+  [first, list]
+end
+
+puts "split:"
+p split( %w{a b c d e f g h i},3)
+
+def slice(list,i,j)
+  list.select_with_index {|x,index| index >= i-1 and index <= j-1}
+end
+
+puts "slice:"
+p slice(%w{a b c d e f g h i j k},3,7)
+
+def rotate(list,n)
+  n.times {list << list.shift}
+  list
+end
+
+puts "rotate:"
+p rotate(%w{a b c d e f g h},3)
+
+def remove_at(list,k)
+  list.reject_with_index {|x,i| i == (k-1)}
+end
+  
+puts "remove:"  
+p remove_at(%w{a b c d},2)
